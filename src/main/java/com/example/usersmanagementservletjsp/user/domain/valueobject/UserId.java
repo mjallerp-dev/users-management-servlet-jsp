b48077d660e15f1ac25c95d31d40cedbc7233ffd
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-08-16 13:17:07 -0500

    feat(user-domain): add user id value object with non-empty validation

---
+++ src/main/java/com/example/usersmanagementservletjsp/user/domain/valueobject/UserId.java
@@ -0,0 +1,24 @@
+package com.example.usersmanagementservletjsp.user.domain.valueobject;
+
+import com.example.usersmanagementservletjsp.user.domain.exceptions.InvalidUserIdException;
+
+import java.util.Objects;
+
+public record UserId (String value) {
+
+    public UserId {final String normalizedValue = Objects.requireNonNull(value, "UserId cannot be null").trim();
+        validateNotEmpty(normalizedValue);
+        value = normalizedValue;
+    }
+
+    public static void validateNotEmpty(final String normalizedValue){
+        if (normalizedValue.isEmpty()){
+            throw InvalidUserIdException.becauseValueIsEmpty();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
