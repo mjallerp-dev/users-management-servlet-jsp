85b6dbd83a870f2821f400327f221b83a931a5ff
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-08-17 00:20:27 -0500

    feat(user-application): add CreateUserCommand

---
+++ src/main/java/com/example/usersmanagementservletjsp/user/application/service/dto/command/CreateUserCommand.java
@@ -0,0 +1,19 @@
+package com.example.usersmanagementservletjsp.user.application.service.dto.command;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserCommand(
+        @NotBlank(message = "id must not be blank") String id,
+        @NotBlank(message = "name must not be blank")
+        @Size(min = 3, message = "name must have at least 3 characters")
+        String name,
+        @NotBlank(message = "email must not be blank")
+        @NotBlank(message = "password must not be blank")
+        @Size(min = 8, message = "password must have at least 8 characters")
+        String password,
+        @NotBlank(message = "role must not be blank") String role)
+{
+
+}
