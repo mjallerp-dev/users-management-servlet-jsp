2456fde0c6f1f12cf84829fa4cd9b0fc7ea8bb7d
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-08-17 21:06:38 -0500

    feat(user-application): add user update command mappings

--- src/main/java/com/example/usersmanagementservletjsp/user/application/service/mapper/UserApplicationMapper.java
+++ src/main/java/com/example/usersmanagementservletjsp/user/application/service/mapper/UserApplicationMapper.java
@@ -1,6 +1,9 @@
 package com.example.usersmanagementservletjsp.user.application.service.mapper;
 
+import com.example.usersmanagementservletjsp.user.application.service.dto.command.ChangeUserPasswordCommand;
 import com.example.usersmanagementservletjsp.user.application.service.dto.command.CreateUserCommand;
+import com.example.usersmanagementservletjsp.user.application.service.dto.command.UpdateUserNameCommand;
+import com.example.usersmanagementservletjsp.user.application.service.dto.command.UpdateUserRoleCommand;
 import com.example.usersmanagementservletjsp.user.domain.enums.UserRole;
 import com.example.usersmanagementservletjsp.user.domain.model.UserModel;
 import com.example.usersmanagementservletjsp.user.domain.valueobject.UserId;
@@ -9,7 +12,7 @@ import com.example.usersmanagementservletjsp.user.domain.valueobject.UserPasswor
 
 public class UserApplicationMapper {
 
-    public UserModel fromCommandToModel(final CreateUserCommand command) {
+    public UserModel fromCreateCommandToModel(final CreateUserCommand command) {
         return UserModel.create(
                 new UserId(command.id()),
                 new UserName(command.name()),
@@ -17,4 +20,29 @@ public class UserApplicationMapper {
                 UserRole.fromString(command.role())
         );
     }
+
+    public UserId fromUpdateNameCommandToUserId(final UpdateUserNameCommand command) {
+        return new UserId(command.id());
+    }
+
+    public UserName fromUpdateNameCommandToUserName(final UpdateUserNameCommand command) {
+        return new UserName(command.name());
+    }
+
+    public UserId fromChangePasswordCommandToUserId(final ChangeUserPasswordCommand command) {
+        return new UserId(command.id());
+    }
+
+    public UserPassword fromChangePasswordCommandToUserPassword (final ChangeUserPasswordCommand command){
+        return UserPassword.fromPlainText(command.password());
+    }
+
+    public UserId fromUpdateUserRoleCommandToUserId (final UpdateUserRoleCommand command){
+        return new UserId(command.id());
+    }
+
+    public UserRole fromUpdateUserRoleCommandToUserRole (final UpdateUserRoleCommand command){
+        return UserRole.fromString(command.role());
+    }
+
 }
